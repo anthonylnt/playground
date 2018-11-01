9a0512a64c680f09f37b735b1834155475332b3d
Anthony Lionnet <lionnet.ant@gmail.com>
2018-11-02 00:35:01 +0100

    Resizing images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   has_many_attached :images
-
   scope :with_eager_loaded_images, -> { eager_load(images_attachment: :blob) }
+
+  def thumbnail input
+    return self.images[input].variant(resize: '300x300!').processed
+  end
 end
